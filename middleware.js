9b42aec6158ec27fc3f6c42a7264329aed5280e3
YILONG WU <yilongwu97@gmail.com>
2022-09-22 20:13:46 -0600

    add admin function

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@ const {campgroundValSchema, reviewValSchema} = require('./valiSchema.js');
 const ExpressError = require('./utils/ExpressError')
 const Campground = require('./models/campground')
 const Review = require('./models/review')
+require('dotenv').config();
 
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()){
@@ -29,7 +30,7 @@ module.exports.validateCampground = (req, res, next) => {
 module.exports.isAuthor = async (req, res, next) => {
     const {id} = req.params;
     const campground = await Campground.findById(id);
-    if(!campground.author.equals(req.user._id)){
+    if(!campground.author.equals(req.user._id) && req.user._id != process.env.ADMIN_ID){
         req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
     }
@@ -39,7 +40,7 @@ module.exports.isAuthor = async (req, res, next) => {
 module.exports.isReviewAuthor = async (req, res, next) => {
     const {id, reviewId} = req.params;
     const review = await Review.findById(reviewId);
-    if(!review.author.equals(req.user._id)){
+    if(!review.author.equals(req.user._id) && req.user._id != process.env.ADMIN_ID){
         req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
     }
